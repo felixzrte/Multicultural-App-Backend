507bec8e2dfe324e841a5481ea43a5b4505589bc
Felix <felixzarate14@gmail.com>
2021-12-02 08:49:58 -0500

    more auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,43 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
 
   res.status(201).json({
     status: 'success',
-    use: newUser,
+    token,
+    user: newUser,
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1.) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  //2.) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  console.log(user);
+  //3.) If everything is okay, send the token to client
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -22,6 +22,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password!'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
