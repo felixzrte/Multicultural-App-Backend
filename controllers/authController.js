1767a225183a7720ac444cbb7cba9347e4e00a95
Felix <felixzarate14@gmail.com>
2021-12-21 14:18:51 -0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -63,8 +63,8 @@ exports.protect = catchAsync(async (req, res, next) => {
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3.) Check if user still exists
-  const freshUser = await User.findById(decoded.id);
-  if (!freshUser) {
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
     return next(
       new AppError(
         'The user belonging to this token does no longer exist!',
@@ -74,13 +74,13 @@ exports.protect = catchAsync(async (req, res, next) => {
   }
 
   // 4.) Check if user changed password after the token was issued
-  if (freshUser.changedPasswordAfter(decoded.iat)) {
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
     return next(
       new AppError('User recently changed password! Please log in again!', 401)
     );
   }
 
-  // Grant access to protected route
-  req.user = freshUser;
+  // Grant access to protected route 
+  req.user = currentUser;
   next();
 });
