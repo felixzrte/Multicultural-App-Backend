331f15173a2d59577d361363a6b299fde13ce9de
Felix <felixzarate14@gmail.com>
2021-12-21 19:12:04 -0500

    Sending Emails with Nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-expressions */
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id: id }, process.env.JWT_SECRET, {
@@ -110,5 +112,35 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false }); // deactivate all the validators that are specified in the schema
 
   // 3.) Send it to the users email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfrim to: ${resetURL}.
+  \nIf you didn't forget you password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 min)',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError(
+        'There was an error sending the email. Try again later!',
+        500
+      )
+    );
+  }
 });
 exports.resetPassword = (res, req, next) => {};
--- utils/email.js
+++ utils/email.js
@@ -1,17 +1,27 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = (options) => {
+const sendEmail = async (options) => {
   // 1.) Create a transporter
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
-      password: process.env.EMAIL_PASSWORD,
+      pass: process.env.EMAIL_PASSWORD,
     },
-    // Activate in gmail "less secure app" option
   });
 
   // 2.) Define the email options
+  const mailOptions = {
+    from: 'Felix Zarate <user@felix.io>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
 
   // 3.) Send email
+  await transporter.sendMail(mailOptions);
 };
+
+module.exports = sendEmail;
