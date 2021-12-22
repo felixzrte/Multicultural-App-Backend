678117e1117b91e3693f0f86ce130c80e364e955
Felix <felixzarate14@gmail.com>
2021-12-21 22:56:09 -0500

    Send JWT via Cookie

--- models/userModel.js
+++ models/userModel.js
@@ -71,7 +71,7 @@ userSchema.pre('save', function (next) {
   next();
 });
 
-userSchema.pre(/^find/, function(next) {
+userSchema.pre(/^find/, function (next) {
   // this points to the current query
   this.find({ active: { $ne: false } });
   next();
