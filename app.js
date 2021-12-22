da092dc2b9bea841f19b7988098dd7bec61745aa
Felix <felixzarate14@gmail.com>
2021-12-21 23:26:38 -0500

    Data Sanitization

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 //NEW STUFF FOR REAL
 const bodyParser = require('body-parser');
@@ -45,6 +47,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`)); // serve static files from folder, not route
